2cff08e13d905af13cf304bb0ca62ec8b4629f85
winningtrendingbots <118413227+winningtrendingbots@users.noreply.github.com>
2025-12-21 19:23:52 +0100

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -9,7 +9,7 @@ import time
 
 # Credenciales
 TELEGRAM_API = os.environ.get('TELEGRAM_API', '')
-CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
+CHAT_ID = os.environ.get('CHAT_ID', '')
 
 print("="*70)
 print("  📱 TEST DE TELEGRAM")
